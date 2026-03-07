591e31dc233bfb73b82ba6e4927400b6fbf1f7ec
Edward Kelly <eddie@sektek.net>
2026-03-07 13:51:17 +0000

    chore: lint fixes and doc updates

--- src/types/cache-events.ts
+++ src/types/cache-events.ts
@@ -1,6 +1,29 @@
 export type CacheEvents<T, K = string> = {
+  /**
+   * Emitted when a value is found in the cache.
+   *
+   * @event CacheEvents#'cache:hit'
+   */
   'cache:hit': (key: K, value: T) => void;
+
+  /**
+   * Emitted when a value is not found in the cache.
+   *
+   * @event CacheEvents#'cache:miss'
+   */
   'cache:miss': (key: K) => void;
+
+  /**
+   * Emitted when a value is set in the cache.
+   *
+   * @event CacheEvents#'cache:set'
+   */
   'cache:set': (key: K, value: T) => void;
+
+  /**
+   * Emitted when a value is deleted from the cache.
+   *
+   * @event CacheEvents#'cache:deleted'
+   */
   'cache:deleted': (key: K) => void;
 };
